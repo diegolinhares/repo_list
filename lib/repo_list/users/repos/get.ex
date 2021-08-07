30624133766c6e829ba9818226feb07da971055b
Diego Linhares <diegomlinhares@gmail.com>
2021-08-07 18:55:39 -0300

    Refactor code to be elixir-ish

--- lib/repo_list/users/repos/get.ex
+++ lib/repo_list/users/repos/get.ex
@@ -2,6 +2,8 @@ defmodule RepoList.Users.Repos.Get do
   def call(username), do: client().get_user_repos(username)
 
   defp client do
-    Application.fetch_env!(:repo_list, __MODULE__)[:github_adapter]
+    :repo_list
+    |> Application.fetch_env!(__MODULE__)
+    |> Keyword.get(:github_adapter)
   end
 end
