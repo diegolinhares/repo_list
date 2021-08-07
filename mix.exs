3704c1c82e547e696de2f88168f475c974d7fbfb
Diego Linhares <diegomlinhares@gmail.com>
2021-08-07 16:35:03 -0300

    Add bypass

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule RepoList.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:tesla, "~> 1.4"},
-      {:hackney, "~> 1.17"}
+      {:hackney, "~> 1.17"},
+      {:bypass, "~> 2.1", only: :test}
     ]
   end
 
