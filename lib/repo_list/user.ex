3ae2eac283efcc210784109882446aec0730df8f
Diego Linhares <diegomlinhares@gmail.com>
2021-08-07 22:37:12 -0300

    Save the password hash to the database

--- lib/repo_list/user.ex
+++ lib/repo_list/user.ex
@@ -2,6 +2,8 @@ defmodule RepoList.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
   @required_fields [:password]
 
   @derive {Jason.Encoder, only: [:id]}
@@ -16,5 +18,12 @@ defmodule RepoList.User do
     |> cast(params, @required_fields)
     |> validate_required(@required_fields)
     |> validate_length(:password, min: 12)
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
