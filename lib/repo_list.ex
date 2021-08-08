1d221fa75caa6324d01c95c83d74e05fc18c2b96
Diego Linhares <diegomlinhares@gmail.com>
2021-08-07 21:34:17 -0300

    Add function to query users by id

--- lib/repo_list.ex
+++ lib/repo_list.ex
@@ -1,5 +1,7 @@
 defmodule RepoList do
   alias RepoList.Users.Repos.Get, as: UserGetRepos
+  alias RepoList.Users.Get, as: UserGet
 
   defdelegate get_user_repos(username), to: UserGetRepos, as: :call
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
